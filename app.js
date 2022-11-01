7fec40765ebfdf6bd8c9950e697d1b1ecb654d51
SIta Dabo <euphoria214rue@gmail.com>
2022-11-01 12:31:04 +0100

    post , Delete And Get Request
    added the create post and delete requesdt

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ const app = express();
 const uri = 'mongodb+srv://engnrobad:bloggr@bloggr.6qgvbcc.mongodb.net/Bloggr?retryWrites=true&w=majority';
 const Blog = require('./models/blog');
 
-
-
-
 mongoose.connect(uri, {UseNewURLParser : true, UseUnifiedTopology : true})
 .then(()=>  app.listen(3000), console.log('Db Connected Successfully'))
 .catch((err)=> console.log(err));
+
+
 
 
 app.set('view engine', 'ejs');
@@ -21,7 +20,7 @@ app.set('views', 'views');
 
 
 app.use(express.static('public'))
-
+app.use(express.urlencoded({extended : true}))
 
 
 
@@ -39,42 +38,74 @@ app.get('/', (req, res)=>{
     .catch((err)=> console.log(err));
   
 })
+
+
+
+
+
+app.post('/', (req, res)=>{
+    const blog = new Blog(req.body);
+
+    blog.save()
+    .then((result)=>{
+        res.redirect('/');
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+
+    })
+
+
+
+
+
+    
+    app.get('/:id', (req, res)=>{
+        const id = req.params.id
+  
+        Blog.findById(id)
+        .then((result)=>{
+          res.render('content', {title : "Blog Details", blog :result})
+        })
+        .catch(err=> console.log(err))
+      })
+
+
+
+
+  
+  app.delete('/:id', (req, res)=>{
+      const id = req.params.id
+      Blog.findByIdAndDelete(id)
+      .then((result)=>{
+          res.json({redirect : '/'})
+      })
+      .catch((err)=>{
+          console.log(err)
+      });
+  
+  })
+
+
+
+
+
 app.get('/blog/create', (req, res)=>{
    res.render('newblog', {title : 'Create a New Blog'})
 
 })
+
+
+
+
 app.get('/home', (req, res)=>{
     res.redirect('/');
     res.statusCode = 301 ;
 })
 
-// Sand Box routes
 
-app.get('/blog-create', (req, res)=>{
-    const blog = new Blog({
-        title : 'This iS My New Blog3', 
-        snippet : 'This is the snippet for Blog 3', 
-        body: ' This is the body for Blog3'
 
-    })
-    blog.save()
-    .then((result)=>{
-        res.send(result)
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
-    
-})
-
-app.get('/all-blogs', (req, res)=>{
-    Blog.find()
-    .then((result)=>{
-        res.send(result);
-    })
-    .catch((err)=> console.log(err))
-    ;
-})
 
 
 
